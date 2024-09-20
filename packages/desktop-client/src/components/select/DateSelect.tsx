98c34cec06242b88f3a4ea4dda5f68487e7347af
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2024-09-19 21:59:57 -0700

    Fix typecheck error

--- packages/desktop-client/src/components/select/DateSelect.tsx
+++ packages/desktop-client/src/components/select/DateSelect.tsx
@@ -1,5 +1,6 @@
 // @ts-strict-ignore
 import React, {
+  type CSSProperties,
   forwardRef,
   useEffect,
   useImperativeHandle,
@@ -36,7 +37,7 @@ import { View } from '../common/View';
 import DateSelectLeft from './DateSelect.left.png';
 import DateSelectRight from './DateSelect.right.png';
 
-const pickerStyles = {
+const pickerStyles: CSSProperties = {
   '& .pika-single.actual-date-picker': {
     color: theme.calendarText,
     background: theme.calendarBackground,
@@ -164,7 +165,7 @@ const DatePicker = forwardRef<DatePickerForwardedRef, DatePickerProps>(
 
     return (
       <View
-        className={css({ ...pickerStyles, flex: 1 })}
+        className={css([pickerStyles, { flex: 1 }])}
         innerRef={mountPoint}
       />
     );
